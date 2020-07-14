2bbbf77c906103b876de46040041cf9ae127a2a5
ipfomin <harry.phomin@gmail.com>
2020-07-14 20:44:07 +0500

    Updates

---
+++ precache-manifest.13eb129782cce3d446a6b9beb8c010d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d6903e3c5de95b3d0f8865c181ab1d",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "41a7976f7713beb8fd01",
+    "url": "/restaurant/static/css/main.e4326c2c.chunk.css"
+  },
+  {
+    "revision": "b6f2508a9d36380cda14",
+    "url": "/restaurant/static/js/2.83a530eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restaurant/static/js/2.83a530eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a7976f7713beb8fd01",
+    "url": "/restaurant/static/js/main.e5893f0b.chunk.js"
+  },
+  {
+    "revision": "bbb8a6d1ae5d7298c65d",
+    "url": "/restaurant/static/js/runtime-main.4f2d4cff.js"
+  }
+]);
